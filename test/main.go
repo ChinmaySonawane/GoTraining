098c3577e4304d852a4f5660e3e6a1af3082294d
Chinmay Sonawane <chinmay.sonawane@joshsoftware.com>
2019-01-21 18:31:17 +0530

    for test comments added

--- test/main.go
+++ test/main.go
@@ -7,14 +7,14 @@ import (
 
 func main() {
 	var urls = []string{"http://www.google.com/", "http://www.chinmay.com/", "http://www.chinmaysonawane.com/", "https://golang.org/", "https://www.joshsoftware.com"}
-	var done = make(chan *website.Website)
+	var done = make(chan *website.Website) //channel to pass CheckStatus func
 	for _, url := range urls {
 		webobj := website.NewWebsite(url)
 
-		go webobj.CheckStatus(done)
+		go webobj.CheckStatus(done) //making of goroutines
 	}
 	for range urls {
-		webobj := <-done
+		webobj := <-done //channel receiver
 		if webobj.Status == true {
 			fmt.Println(webobj.Url, "is working")
 			continue
--- test/website/website.go
+++ test/website/website.go
@@ -4,22 +4,25 @@ import (
 	"net/http"
 )
 
+//struct for website to hold result
 type Website struct {
 	Url    string
 	Status bool
 }
 
+//constructor for Website
 func NewWebsite(url string) *Website {
 	return &Website{url, false}
 }
 
+//CheckStatus is func call on Website take chan *Website to pass status
 func (w *Website) CheckStatus(done chan *Website) {
 	_, err := http.Get(w.Url)
 	if err != nil {
 		w.Status = false
-		done <- w
+		done <- w //passing ptr to Website struct in chan
 		return
 	}
-	done <- w
+	done <- w //passing ptr to Website struct in chan
 	w.Status = true
 }
